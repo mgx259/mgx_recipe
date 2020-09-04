025b00ed63f004c9c0cc13fd271b6fcf05b67f92
Maksim <mgx2@inbox.lv>
2020-09-05 00:32:02 +0300

    Add private API test and user update functionality

--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,4 @@
-# from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -15,3 +14,15 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = UserSerializer
+    # queryset = ''
+
+    def get_object(self):
+        """Retrieve and return an authenticated user"""
+        return self.request.user
